54fce84dfc4881ec7461744650695ff44bfe9103
Cory Forsyth <cory.forsyth@gmail.com>
2010-07-29 17:07:41 -0400

    Add some styling

--- is_it_a_haiku_app.rb
+++ is_it_a_haiku_app.rb
@@ -8,6 +8,7 @@
 class IsItAHaikuApp < Sinatra::Application
   require "lib/haiku"
   require "haml"
+  require "sass"
   
   @@haikus = $mongo.collection("haikus")
   
@@ -36,14 +37,9 @@ class IsItAHaikuApp < Sinatra::Application
       haml :haiku
     end
   end
-
-  # Test at <appname>.heroku.com
-
-  # You can see all your app specific information this way.
-  # IMPORTANT! This is a very bad thing to do for a production
-  # application with sensitive information
-
-  # get '/env' do
-  #   ENV.inspect
-  # end
+  
+  get '/stylesheet.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :stylesheet
+  end
 end
